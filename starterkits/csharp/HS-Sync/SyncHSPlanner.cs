52ddfe1670eac4b1ffe535da55cced9a329b01c8
Maximilian Mitter <p42674@fhooe.at>
2022-05-06 13:09:30 +0200

    Adds option to run a synchronous solver using a req/res socket pattern. Adds a beamsearch algorithm as an example.

---
+++ starterkits/csharp/HS-Sync/SyncHSPlanner.cs
@@ -0,0 +1,33 @@
+﻿using DynStacking;
+using DynStacking.HotStorage.DataModel;
+using Google.Protobuf;
+using Google.Protobuf.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp.HS_Sync {
+    public class SyncHSPlanner : IPlanner {
+        private int seqNr = 0;
+
+        public byte[] PlanMoves(byte[] worldData, OptimizerType opt) {
+            return PlanMoves(World.Parser.ParseFrom(worldData), opt)?.ToByteArray();
+        }
+
+        private CraneSchedule PlanMoves(World world, OptimizerType opt) {
+            if (world.Buffers == null || (world.Crane.Schedule.Moves?.Count ?? 0) > 0) return null;
+
+            var schedule = new CraneSchedule() { SequenceNr = seqNr++ };
+            var initial = new RFState(world);
+            var solution = initial.GetBestMovesBeam(new List<CraneMove>(), 6, 5);
+            var list = solution.Item1.ConsolidateMoves();
+            if (solution != null)
+                schedule.Moves.AddRange(list.Take(3)
+                                        .TakeWhile(move => world.Handover.Ready || move.TargetId != world.Handover.Id));
+
+            return schedule.Moves.Count > 0 ? schedule : null;
+        }
+    }
+}
--- starterkits/csharp/Program.cs
+++ starterkits/csharp/Program.cs
@@ -3,64 +3,106 @@ using System.Text;
 using NetMQ;
 using NetMQ.Sockets;
 using Google.Protobuf;
+using DynStacking.HotStorage.DataModel;
+using System.Diagnostics;
 
 namespace DynStacking {
-  public enum OptimizerType {
-    RuleBased,
-    ModelBased
-  }
-  public enum ProblemType {
-    HotStorage,
-    RollingMill
-  }
-
-  interface IPlanner {
-    byte[] PlanMoves(byte[] worldData, OptimizerType opt);
-  }
-
-  class Program {
-    static void Main(string[] args) {
-      if (args.Length < 3) {
-        Console.WriteLine("Requeries 2 arguments: SOCKET SIM_ID PROBLEM");
-        return;
-      }
-      var socketAddr = args[0];
-      var identity = new UTF8Encoding().GetBytes(args[1]);
-      IPlanner planner = args[2] == "HS" ? new HotStorage.Planner() : new RollingMill.Planner();
-
-      OptimizerType optType;
-      if (args.Length > 2) {
-        optType = OptimizerType.ModelBased;
-      } else {
-        optType = OptimizerType.RuleBased;
-      }
-      Console.WriteLine(optType);
-
-      using (var socket = new DealerSocket()) {
-        socket.Options.Identity = identity;
-        socket.Connect(socketAddr);
-        Console.WriteLine("Connected");
-
-
-        while (true) {
-
-          var answer = planner.PlanMoves(socket.ReceiveMultipartBytes()[2], optType);
-
-          if (answer != null) {
-            var msg = new NetMQMessage();
-            msg.AppendEmptyFrame();
-            msg.Append("crane");
-            msg.Append(answer);
-            socket.SendMultipartMessage(msg);
-          }
-
-        }
-
-      }
-
+    public enum OptimizerType {
+        RuleBased,
+        ModelBased
+    }
+    public enum ProblemType {
+        HotStorage,
+        RollingMill
     }
 
+    interface IPlanner {
+        byte[] PlanMoves(byte[] worldData, OptimizerType opt);
+    }
 
+    class Program {
+        static void Main(string[] args) {
+            if (args.Length < 3) {
+                Console.WriteLine("Requieres 3 arguments: SOCKET SIM_ID PROBLEM");
+                return;
+            }
+            var socketAddr = args[0];
+            var identity = new UTF8Encoding().GetBytes(args[1]);
+            //IPlanner planner = args[2] == "HS" ? new HotStorage.Planner() : new RollingMill.Planner();
+            IPlanner planner = args[2] == "HS" ? new csharp.HS_Sync.SyncHSPlanner() : new RollingMill.Planner();
 
-  }
+            OptimizerType optType;
+            if (args.Length > 2) {
+                optType = OptimizerType.RuleBased;
+            } else {
+                optType = OptimizerType.ModelBased;
+            }
+            bool runSynchronously = false;
+            if (args.Length >= 4) {
+                if (args[3] == "sync") {
+                    runSynchronously = true;
+                } else {
+                    runSynchronously = false;
+                }
+            }
+
+            Console.WriteLine(optType);
+            if (runSynchronously) {
+                Console.WriteLine("Running solver syncronously");
+                using (var socket = new ResponseSocket()) {
+                    socket.Options.Identity = identity;
+                    socket.Bind(socketAddr);
+                    Console.WriteLine($"Connected Solver to address {socketAddr}");
+                    int nextValidStep = 0, currentStep = 0;
+                    while (true) {
+                        var request = socket.ReceiveMultipartMessage();
+                        var id = request.Pop().ConvertToString();
+                        if (id != args[1]) {
+                            socket.SendFrameEmpty(false);
+                            continue;
+                        }
+                        request.Pop(); //empty delim frame
+                        var skipSteps = Math.Floor((int.Parse(request.Pop().ConvertToString().Split(' ')[1])) / 1000d);
+
+                        if (skipSteps > 0 && nextValidStep < currentStep) {
+                            nextValidStep = currentStep + (int)skipSteps;
+                        }
+                        if (nextValidStep == currentStep) {
+                            var answer = planner.PlanMoves(request.Pop().Buffer, optType);
+                            if (answer != null) {
+                                socket.SendFrame(identity, true);
+                                socket.SendFrameEmpty(true);
+                                socket.SendFrame($"crane", true);
+                                socket.SendFrame(answer, false);
+                            } else {
+                                socket.SendFrameEmpty(false);
+                            }
+                            nextValidStep++;
+                        } else {
+                            socket.SendFrameEmpty();
+                        }
+                        currentStep++;
+                    }
+                }
+            } else {
+                using (var socket = new DealerSocket()) {
+                    socket.Options.Identity = identity;
+                    socket.Connect(socketAddr);
+                    Console.WriteLine("Connected");
+
+                    while (true) {
+                        var answer = planner.PlanMoves(socket.ReceiveMultipartBytes()[2], optType);
+
+                        if (answer != null) {
+                            var msg = new NetMQMessage();
+                            msg.AppendEmptyFrame();
+                            msg.Append("crane");
+                            msg.Append(answer);
+                            socket.SendMultipartMessage(msg);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
