7819df8b6cf9ef8c1fb138cc4663d0f994e16475
Maximilian Mitter <p42674@fhooe.at>
2022-05-12 10:37:39 +0200

    Removes changes to simplify solver use

--- starterkits/csharp/Program.cs
+++ starterkits/csharp/Program.cs
@@ -47,62 +47,28 @@ namespace DynStacking {
       }
 
       Console.WriteLine(optType);
-      if (runSynchronously) {
-        Console.WriteLine("Running solver synchronously");
-        using (var socket = new ResponseSocket()) {
-          socket.Options.Identity = identity;
-          socket.Bind(socketAddr);
-          Console.WriteLine($"Connected Solver to address {socketAddr}");
-          int nextValidStep = 0, currentStep = 0;
-          while (true) {
-            Console.WriteLine("Listening for Message");
-            var request = socket.ReceiveMultipartMessage();
-            Console.WriteLine("Message Received");
-            var id = request.Pop().ConvertToString();
-            if (id != args[1]) {
-              socket.SendFrameEmpty(false);
-              continue;
-            }
-            request.Pop(); //empty delim frame
-            var skipSteps = Math.Floor((int.Parse(request.Pop().ConvertToString().Split(' ')[1])) / 1000d);
 
-            if (skipSteps > 0 && nextValidStep < currentStep) {
-              nextValidStep = currentStep + (int)skipSteps;
-            }
-            if (nextValidStep == currentStep) {
-              var answer = planner.PlanMoves(request.Pop().Buffer, optType);
-              if (answer != null) {
-                socket.SendFrame(identity, true);
-                socket.SendFrameEmpty(true);
-                socket.SendFrame($"crane", true);
-                socket.SendFrame(answer, false);
-              } else {
-                socket.SendFrameEmpty(false);
-              }
-              nextValidStep++;
-            } else {
-              socket.SendFrameEmpty(false);
-            }
-            currentStep++;
-          }
-        }
-      } else {
-        using (var socket = new DealerSocket()) {
-          socket.Options.Identity = identity;
-          socket.Connect(socketAddr);
-          Console.WriteLine("Connected");
+      using (var socket = new DealerSocket()) {
+        socket.Options.Identity = identity;
+        socket.Connect(socketAddr);
+        Console.WriteLine("Connected");
 
-          while (true) {
-            var answer = planner.PlanMoves(socket.ReceiveMultipartBytes()[2], optType);
+        while (true) {
+          Console.WriteLine("Waiting for request...");
+          var request = socket.ReceiveMultipartBytes();
+          Console.WriteLine("Incoming request");
+          var answer = planner.PlanMoves(request[2], optType);
 
-            if (answer != null) {
-              var msg = new NetMQMessage();
-              msg.AppendEmptyFrame();
-              msg.Append("crane");
-              msg.Append(answer);
-              socket.SendMultipartMessage(msg);
-            }
-          }
+          var msg = new NetMQMessage();
+          //msg.Append(request[0]);
+          msg.AppendEmptyFrame();
+          msg.Append("crane");
+          if (answer != null)
+            msg.Append(answer);
+          else
+            msg.AppendEmptyFrame();
+
+          socket.SendMultipartMessage(msg);
         }
       }
     }
